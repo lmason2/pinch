adcf1e73f84bbdc8002551283cccaea28315b8c4
Luke Mason <lukesamuelmason@gmail.com>
2021-12-26 18:43:31 -0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Luke Mason on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
